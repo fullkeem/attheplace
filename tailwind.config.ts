577c0e18d2c871f1d8b6e1b2a9dd1f87fae08097
Choongman Kim <cm730712@gmail.com>
2024-10-12 23:39:51 +0900

    Add: 메뉴 아이콘, 로고 이미지 및 페이지 배경 이미지 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,16 @@ const config: Config = {
         mobile: "375px",
         desktop: "1024px",
       },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(180deg, #EBEEF2 0%, #CBCFD6 15%, #99A0A9 30%, #777E88 45%, #545961 60%, #383B40 75%, #1D1F21 100%)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-inria)", "sans-serif"],
       },
     },
   },
