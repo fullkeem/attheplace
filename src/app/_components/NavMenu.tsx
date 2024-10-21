4abe242fe837bbefc70a2d73530b062844ab9e96
Choongman Kim <cm730712@gmail.com>
2024-10-22 01:54:59 +0900

    [Feat] findingCafe 페이지 API 통신 기능 구현

--- src/app/_components/NavMenu.tsx
+++ src/app/_components/NavMenu.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import Link from "next/link";
-import Image from "next/image";
-import classNames from "classnames";
-import { useState, useEffect } from "react";
-import arrow from "/public/icons/menuArrow.svg";
+import axios from 'axios';
+import Link from 'next/link';
+import Image from 'next/image';
+import classNames from 'classnames';
+import { useState, useEffect } from 'react';
+import arrow from '/public/icons/menuArrow.svg';
 
 interface UserInfo {
   email: string;
@@ -25,8 +25,8 @@ export default function Menu() {
   const menuClasses = classNames(
     "fixed top-0 right-0 w-full h-full z-20 transform transition-transform duration-300 bg-[url('/images/blackBg.webp')] bg-cover bg-center",
     {
-      "translate-x-0": isMenuOpen,
-      "translate-x-full": !isMenuOpen,
+      'translate-x-0': isMenuOpen,
+      'translate-x-full': !isMenuOpen,
     }
   );
 
@@ -34,10 +34,10 @@ export default function Menu() {
     const fetchUserInfo = async () => {
       try {
         // localStorage에서 저장된 토큰을 가져오기
-        const token = JSON.parse(localStorage.getItem("token") || "null");
+        const token = JSON.parse(localStorage.getItem('token') || 'null');
         // 토큰이 없으면 함수 실행 중지
         if (!token || !token.accessToken) {
-          console.log("no token");
+          console.log('no token');
           return;
         }
         console.log(token.accessToken);
@@ -55,14 +55,14 @@ export default function Menu() {
         // 받아온 사용자 정보 저장
         setUserInfo(response.data);
 
-        console.log("유저 정보:", userInfo);
+        console.log('유저 정보:', response.data);
       } catch (error: unknown) {
-        console.error("사용자 정보 불러오기 실패:", error);
+        console.error('사용자 정보 불러오기 실패:', error);
       }
     };
 
     fetchUserInfo();
-  }, [userInfo]);
+  }, []);
 
   return (
     <>
@@ -72,10 +72,10 @@ export default function Menu() {
 
       {/* 네비게이션 메뉴 */}
       <nav className={menuClasses}>
-        <button className="absolute top-6 right-6" onClick={toggleMenu}>
+        <button className="absolute right-6 top-6" onClick={toggleMenu}>
           X
         </button>
-        <ul className="mt-14 flex flex-col p-6 gap-7">
+        <ul className="mt-14 flex flex-col gap-7 p-6">
           <li className="py-1">
             <Link href="/login" onClick={toggleMenu} className="flexBetween">
               <div>로그인</div>
@@ -110,7 +110,7 @@ export default function Menu() {
       {/* 메뉴가 열릴 때 어두운 배경 추가 */}
       {isMenuOpen && (
         <div
-          className="fixed inset-0 bg-black opacity-50 z-10"
+          className="fixed inset-0 z-10 bg-black opacity-50"
           onClick={toggleMenu}
         ></div>
       )}
--- src/app/findingCafe/page.tsx
+++ src/app/findingCafe/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import axios from 'axios';
 import ProgressBar from '../_components/ProgressBar';
 import { useProgressBarStore } from '../store/store';
 import { useState } from 'react';
@@ -8,12 +9,12 @@ const questions = [
   {
     id: 1,
     question: '반려동물 동반이 가능한 카페를 찾으시나요?',
-    key: 'pets',
+    key: 'pet',
   },
   {
     id: 2,
     question: '단체석(5인 이상)이 가능한 카페를 찾으시나요?',
-    key: 'groupSeat',
+    key: 'group_seat',
   },
   {
     id: 3,
@@ -23,15 +24,22 @@ const questions = [
   {
     id: 4,
     question: '디카페인 커피가 있는 카페를 찾으시나요?',
-    key: 'decaf',
+    key: 'decaffe',
   },
 ];
+
+interface Answers {
+  pet?: boolean;
+  group_seat?: boolean;
+  terrace?: boolean;
+  decaffe?: boolean;
+}
 
 export default function findingCafe() {
   const currentStep = useProgressBarStore((state) => state.currentStep);
   const setCurrentStep = useProgressBarStore((state) => state.setCurrentStep);
 
-  const [answers, setAnswers] = useState({});
+  const [answers, setAnswers] = useState<Answers>({});
 
   const handleAnswer = (key: string, value: boolean) => {
     setAnswers((prev) => ({ ...prev, [key]: value }));
@@ -48,7 +56,24 @@ export default function findingCafe() {
     }
   };
 
-  console.log(answers);
+  const queryParams = Object.keys(answers)
+    .filter((key) => answers[key as keyof Answers])
+    .map((key) => `${key}=1`)
+    .join('&');
+
+  const fetchFindingCafeData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:10010/prefers/?${queryParams}`
+      );
+
+      console.log('결과 :', response.data);
+    } catch (error) {
+      console.error('에러 발생', error);
+    }
+  };
+
+  const allAnswered = Object.keys(answers).length === questions.length;
 
   return (
     <div className="flex w-full flex-col items-center">
@@ -89,6 +114,14 @@ export default function findingCafe() {
               </div>
             </div>
           )
+      )}
+      {allAnswered && (
+        <button
+          onClick={fetchFindingCafeData}
+          className="mt-10 w-full rounded bg-orange-600 p-4 text-white"
+        >
+          결과 확인
+        </button>
       )}
     </div>
   );
