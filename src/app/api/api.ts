17c06764198bf9b72eaea6845a0588484d0fccdd
fullkeem <cm730712@gmail.com>
2024-11-09 01:45:23 +0000

    [Modify]api 통신 주소 변경

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3003',
+  baseURL: 'http://3.34.196.236:3003',
   headers: {
     'Content-Type': 'application/json',
   },
