c71c0b3ff60ad0ef00d01ddece515f5d7ad640ed
fullkeem <cm730712@gmail.com>
2024-12-26 10:17:59 +0000

    baseURL 재수정정

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://attheplace.store:3003',
+  baseURL: 'https://attheplace.store',
   headers: {
     'Content-Type': 'application/json',
   },
